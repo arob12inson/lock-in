5b6433fd0ff8c7dee6e893c08d5ed220c6d7e0db
Aidan Robinson <120530116+arob12inson@users.noreply.github.com>
2025-03-23 23:15:45 -0700

    added basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import "./App.css"
 function App() {
 
   return (
-    <>
-      <p>
-        "Hi!"
-      </p>
-    </>
+    <div className="pomodoro_container">
+      <div className="pomodoro">
+        
+        <p className="timer">12:00</p>
+      </div>
+    </div>
   )
 }
 

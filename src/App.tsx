363d1dee9741e724f3499efb682dfb2c8ebad9f2
Aidan Robinson <120530116+arob12inson@users.noreply.github.com>
2025-03-24 23:33:41 -0700

    Added pomodoro session types

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ function App() {
     <div className="pomodoro_container">
       <div className="pomodoro">
         <div className="timer_options">
-          <button className="timer_option">Focus</button>
+          <button className="timer_option">Focus Time</button>
           <button className="timer_option">Short Break</button>
           <button className="timer_option">Long Break</button>
         </div>
-        <p className="timer">12:00</p>
-        <button className="play_button">Play</button>
+        <p className="timer">50:00</p>
+        <button className="play_button">Start</button>
       </div>
     </div>
   )

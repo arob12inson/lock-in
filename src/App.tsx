492ffb752719ed30bdab6e8ee765b925754b2fca
Aidan Robinson <120530116+arob12inson@users.noreply.github.com>
2025-03-24 21:00:20 -0700

    pomodoro play button + menu bar

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@ function App() {
   return (
     <div className="pomodoro_container">
       <div className="pomodoro">
-        
+        <p className="timer_options">12:00</p>
         <p className="timer">12:00</p>
+        <button className="play_button">Play</button>
       </div>
     </div>
   )

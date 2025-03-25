a4c446cdae2e3323796d7b665c4e43c58904a63f
Aidan Robinson <120530116+arob12inson@users.noreply.github.com>
2025-03-24 22:43:07 -0700

    rounded corners + changing colors

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ function App() {
   return (
     <div className="pomodoro_container">
       <div className="pomodoro">
-        <p className="timer_options">12:00</p>
+        <div className="timer_options">
+          <button className="timer_option">Focus</button>
+          <button className="timer_option">Short Break</button>
+          <button className="timer_option">Long Break</button>
+        </div>
         <p className="timer">12:00</p>
         <button className="play_button">Play</button>
       </div>

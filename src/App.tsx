3446a63c8d870b6d07eda7f5c9cac3271630f1f1
Aidan Robinson <120530116+arob12inson@users.noreply.github.com>
2025-03-26 21:21:51 -0700

    Basic time display logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { useState } from "react"
 import "./App.css"
 function App() {
+
+  const [seconds, setSeconds] = useState(50*60);
+  const [active, setActive] = useState(false);
+  
+  const getTime = (sec: number) => {
+    const seconds = sec % 60;
+    const minutes = Math.floor(sec / 60);
+
+    return `${minutes < 10 ? "0" : ""}${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  }
 
   return (
     <div className="pomodoro_container">
@@ -9,8 +20,8 @@ function App() {
           <button className="timer_option">Short Break</button>
           <button className="timer_option">Long Break</button>
         </div>
-        <p className="timer">50:00</p>
-        <button className="play_button">Start</button>
+        <p className="timer">{getTime(seconds)}</p>
+        <button className="play_button" onClick={() => setActive(!active)}>Start</button>
       </div>
     </div>
   )
